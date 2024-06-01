c7e3dca216a98e9aeb7e809148ff1f8ad53340ef
Sushma Kolli <sushmakolli1510@gmail.com>
2024-06-01 14:28:06 -0400

    first project in git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tic_Tac_Toe from './Components/Tic_Tac_Toe/Tic_Tac_Toe';
+
+
+const App = () => {
+  return (
+    <div>
+      <Tic_Tac_Toe />
+    </div>
+  )
+}
+
+export default App
